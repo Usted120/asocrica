82df4ccdaea0d4e8514b5d20cd75c79df5af0259
Usted120 <rolandoalvarez120@gmail.com>
2022-01-27 13:12:14 -0400

    Responsive for nav

--- JS/menu.js
+++
@@ -1,5 +0,0 @@
-const nav = document.querySelector('.nav_bar');
-
-	window.addEventListener('scroll',function){
-			nav_bar.classList.toggle('active', window.scrollY > 0)
-	}
---
+++ js/menu.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector('.nav_bar');
+const menu_btn = document.querySelector('.menu_btn');
+const menu = document.querySelector('.menu');
+
+	window.addEventListener('scroll',function(){
+			nav.classList.toggle('active', window.scrollY >0)
+	})
+
+	menu_btn.addEventListener('click', () =>{
+		menu.classList.toggle('.active')
+	})
